4529479e86921e7448d981538fc82000690d32f0
aeisses <aaron.eisses@teamspace.ca>
2013-12-11 22:29:01 -0400

    Did a little polish to the code.

--- Android/KNOWtime/src/com/knowtime/AboutActivity.java
+++ Android/KNOWtime/src/com/knowtime/AboutActivity.java
@@ -1,7 +1,19 @@
 package com.knowtime;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
 public class AboutActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+	
+	public void touchBackButton(View view)
+	{
+		this.finish();
+	}
 }
--- Android/KNOWtime/src/com/knowtime/StopsActivity.java
+++ Android/KNOWtime/src/com/knowtime/StopsActivity.java
@@ -1,7 +1,19 @@
 package com.knowtime;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
 public class StopsActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_stops);
+	}
+	
+	public void touchBackButton(View view)
+	{
+		this.finish();
+	}
 }
