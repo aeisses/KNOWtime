927e24ad4af9b45216987b53aa2e446fae785cfa
aeisses <aaron.eisses@teamspace.ca>
2013-12-16 22:50:03 -0400

    Did some more UI work. Cleaned up some screens. Created some style, a clickable button. Moving along.

--- Android/KNOWtime/gen/com/knowtime/R.java
+++ Android/KNOWtime/gen/com/knowtime/R.java
@@ -10,6 +10,9 @@ package com.knowtime;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int backgroundcolour=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,14 +20,43 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int backbutton=0x7f020000;
+        public static final int backgroundonmyway=0x7f020001;
+        public static final int conntectedtoserver=0x7f020002;
+        public static final int facebookbutton=0x7f020003;
+        public static final int feedbackbutton=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int onmyway_button=0x7f020006;
+        public static final int onmywaybutton=0x7f020007;
+        public static final int onmywayimage=0x7f020008;
+        public static final int sending=0x7f020009;
+        public static final int sendingline=0x7f02000a;
+        public static final int stop=0x7f02000b;
+        public static final int twitterbutton=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int aboutBodyView=0x7f090002;
+        public static final int aboutTextView=0x7f090001;
+        public static final int action_settings=0x7f090011;
+        public static final int backButton=0x7f090006;
+        public static final int backbutton=0x7f09000a;
+        public static final int connecttoserveriamge=0x7f090010;
+        public static final int facebookButton=0x7f090004;
+        public static final int feedbackButton=0x7f090005;
+        public static final int imageButton1=0x7f090000;
+        public static final int imageButton2=0x7f090007;
+        public static final int imageButton3=0x7f090008;
+        public static final int imageButton4=0x7f090009;
+        public static final int map1=0x7f09000b;
+        public static final int onmywaybutton=0x7f09000f;
+        public static final int onmywayimage=0x7f09000c;
+        public static final int sendingimage=0x7f09000d;
+        public static final int sendinglineimage=0x7f09000e;
+        public static final int twitterButton=0x7f090003;
     }
     public static final class layout {
         public static final int activity_about=0x7f030000;
@@ -35,14 +67,27 @@ public final class R {
         public static final int activity_twitter=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int about=0x7f060003;
+        public static final int about_label_contentdescription=0x7f060004;
+        public static final int about_textview_contentdescription=0x7f060005;
+        public static final int about_view=0x7f060006;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int back_button_contentdescription=0x7f060002;
+        public static final int connect_to_server_image=0x7f06000e;
+        public static final int facebook_button=0x7f060009;
+        public static final int feedback_button=0x7f060008;
+        public static final int onmy_image=0x7f06000a;
+        public static final int onmyway_button_image=0x7f06000d;
+        public static final int sending_image=0x7f06000c;
+        public static final int sendingline_image=0x7f06000b;
+        public static final int twitter_button=0x7f060007;
     }
     public static final class style {
+        public static final int ActivityTheme=0x7f070002;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -52,22 +97,11 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- Android/KNOWtime/src/com/knowtime/MainActivity.java
+++ Android/KNOWtime/src/com/knowtime/MainActivity.java
@@ -1,10 +1,10 @@
 package com.knowtime;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.android.gms.maps.CameraUpdateFactory;
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.MapFragment;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -12,26 +12,26 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-	private GoogleMap map;
-    LatLng defaultHalifaxLatLng = new LatLng(44.67600, -63.60800);
+//	private GoogleMap map;
+//    LatLng defaultHalifaxLatLng = new LatLng(44.67600, -63.60800);
     int defaultHalifaxLatLngZoom = 7;
 
 
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+//		setContentView(R.layout.activity_main);
 		
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map1)).getMap();
-
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultHalifaxLatLng, defaultHalifaxLatLngZoom));
+//        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map1)).getMap();
+//
+//        map.setMyLocationEnabled(true);
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultHalifaxLatLng, defaultHalifaxLatLngZoom));
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+//		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
