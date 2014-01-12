10c80c9f6fe299fe9f496883f3773f31ec156f6f
aeisses <aaron.eisses@teamspace.ca>
2014-01-12 13:36:49 -0400

    Finished the share me menu image revert

--- Android/KNOWtime/src/com/knowtime/WebApiService.java
+++ Android/KNOWtime/src/com/knowtime/WebApiService.java
@@ -1,6 +1,5 @@
 package com.knowtime;
 
-import android.database.Cursor;
 import android.location.Location;
 import android.text.format.DateFormat;
 import android.util.Log;
@@ -18,10 +17,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,7 +24,6 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
 
