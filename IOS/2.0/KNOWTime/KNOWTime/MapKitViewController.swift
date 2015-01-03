f1d66867016be53940ec3b27452744e5d06b105c
Aaron Eisses <aaron.eisses@gmail.com>
2015-01-03 15:28:08 -0400

    Beginning the connection to the server through web sockets.....

--- IOS/2.0/KNOWTime/KNOWTime/MapKitViewController.swift
+++ IOS/2.0/KNOWTime/KNOWTime/MapKitViewController.swift
@@ -10,25 +10,70 @@ import Foundation
 import MapKit
 import UIKit
 
-class MapKitViewController: UIViewController {
-  
-//  let webSocketClient:WebSocketClient
- 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  override func viewDidAppear(animated: Bool) {
-    super.viewDidAppear(animated);
-    // Do any additional setup after loading the view, typically from a nib.
-    let serverUrl = NSURL(string: "https://www.espn.com")
-    let webSocketClient = WebSocketClient(url: serverUrl!)
-  }
+class MapKitViewController: UIViewController, WebSocketClientDelegate, MKMapViewDelegate {
 
+    required init(coder aDecoder: NSCoder) {
+        let serverUrl = NSURL(string: serverUrlString)
+        self.webSocketClient = WebSocketClient(url: serverUrl!)
+        self.annotationsArray = NSMutableArray()
+        super.init(coder: aDecoder);
+    }
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let serverUrlString = "https://afternoon-eyrie-7322.herokuapp.com"
+    var webSocketClient:WebSocketClient
+    let annotationsArray:NSMutableArray
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.webSocketClient.delegate = self;
+        // Do any additional setup after loading the view, typically from a nib.
+        let location = CLLocationCoordinate2D(
+            latitude: 44.64745,
+            longitude: -63.601491
+        )
+
+        let span = MKCoordinateSpanMake(0.014200, 0.011654)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+  
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated);
+        // Do any additional setup after loading the view, typically from a nib.
+        webSocketClient.openWebSocketClient()
+    }
+    
+    func process(response: NSDictionary) {
+        let latitude:CLLocationDegrees = response["latitude"] as CLLocationDegrees
+        let longitude:CLLocationDegrees = response["longitide"] as CLLocationDegrees
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var information = MKPointAnnotation()
+        information.coordinate = location
+        information.title = response["tripId"] as NSString
+        var foundValue:Bool = false
+        for element in self.annotationsArray {
+            var annotation:MKPointAnnotation = element as MKPointAnnotation
+            if (annotation.title == information.title) {
+//                mapView.removeAnnotation(annotation)
+                annotationsArray.removeObject(annotation)
+                annotationsArray.addObject(information)
+//                mapView.addAnnotation(annotation)
+                foundValue = true
+            }
+        }
+        if (!foundValue) {
+            self.annotationsArray.addObject(information)
+//            mapView.addAnnotation(information);
+        }
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.addAnnotations(self.annotationsArray)
+    }
+    
 }
--- IOS/2.0/KNOWTime/KNOWTime/WebSocketClient.swift
+++ IOS/2.0/KNOWTime/KNOWTime/WebSocketClient.swift
@@ -7,46 +7,75 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+protocol WebSocketClientDelegate {
+    func process(response: NSDictionary)
+}
 
 class WebSocketClient: NSObject, SRWebSocketDelegate {
   
-  var url: NSURL
-  let srWebSocket:SRWebSocket
-  
-  init(url: NSURL) {
-    self.url = url
-    self.srWebSocket = SRWebSocket(URL: url)
-  }
-  
-  func openWebSocketClient() {
-    srWebSocket.open()
-  }
-  
-  func closeWebSocketClient() {
-    srWebSocket.close()
-  }
-  
-  func sendData(data: NSData) {
-    srWebSocket.send(data)
+    var url: NSURL
+    var srWebSocket:SRWebSocket
+    var delegate: WebSocketClientDelegate?
     
-  }
-
-  // SRWebSocket Delegate method functions
-  // Required
-  func webSocket(webSocket: SRWebSocket!, didReceiveMessage message: AnyObject) {
+    init(url: NSURL) {
+        self.url = url
+        self.srWebSocket = SRWebSocket(URL: url)
+        super.init()
+        srWebSocket.delegate = self;
+    }
+  
+    func initializeWebSocketClient() {
+    }
     
-  }
+    func openWebSocketClient() {
+        srWebSocket.open()
+    }
 
-  // Optional
-  func webSocketDidOpen(webSocket: SRWebSocket) {
-    
-  }
+    func closeWebSocketClient() {
+        srWebSocket.close()
+    }
 
-  func webSocket(webSocket: SRWebSocket!, didFailWithError error: NSError) {
-    println(error)
-  }
+    func sendData(data: NSString) {
+        srWebSocket.send(data)
+    }
 
-  func webSocket(webSocket: SRWebSocket!, didCloseWithCode code: NSInteger!, reason: NSString!, wasClean: ObjCBool) {
-    
-  }
+    // SRWebSocket Delegate method functions
+    // Required
+    func webSocket(webSocket: SRWebSocket!, didReceiveMessage message: AnyObject) {
+        println("Websocket message: ")
+        println(message)
+        // Need to take string to data to JSON
+        var parseError: NSError?
+        let data = message.dataUsingEncoding(NSUTF8StringEncoding);
+        let parsedObject: NSDictionary = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: &parseError) as NSDictionary
+        self.delegate?.process(parsedObject)
+//        var getRequest: Dictionary<String, String> = [:];
+//        getRequest["action"] = "GET"
+//        getRequest["type"] = "kGetRouteByRouteId"
+//        getRequest["route"] = "90"
+//        
+//        var parseError: NSError?
+//        let data: NSData? = NSJSONSerialization.dataWithJSONObject(getRequest, options: NSJSONWritingOptions.PrettyPrinted, error:&parseError)
+//        
+//        let resstr = NSString(data: data!, encoding: NSUTF8StringEncoding)
+//        self.sendData(resstr!)
+    }
+
+    // Optional
+    func webSocketDidOpen(webSocket: SRWebSocket) {
+        println("WebSocket did open")
+
+    }
+
+    func webSocket(webSocket: SRWebSocket!, didFailWithError error: NSError) {
+        println("WebSocket error: ")
+        println(error)
+    }
+
+    func webSocket(webSocket: SRWebSocket!, didCloseWithCode code: NSInteger!, reason: NSString!, wasClean: ObjCBool) {
+        println("Reason, socket closed")
+        println(reason)
+    }
 }
